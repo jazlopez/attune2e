5065483d1d20722237a7d0aaadd21f35ecad607f
JAZIEL LOPEZ <jaziel.lopez@thermofisher.com>
2018-02-16 16:22:41 -0800

    log/error function implementation

--- e2e.py
+++ e2e.py
@@ -1,14 +1,24 @@
 # Jaziel Lopez
+from colorama import init
+from colorama import Fore
+from colorama import Back
 from selenium import webdriver
 from selenium.webdriver.common.by import  By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from time import sleep
 
+init()
 
-def log(of, total,message):
 
-    print("[{}/{}] {}".format(of, total, message))
+def error(message):
+
+    print(Fore.RED + message)
+
+
+def log(of, out, message):
+
+    print(Fore.BLUE + "[{}/{}] {}".format(of, out, message))
 
 
 config = {
@@ -51,56 +61,68 @@ try:
 
     submit.click()
 
+    sleep(5)
+
     passed += 1
-    log(passed, total, "User has been logged in successfully")
+    log(passed, total, "User has been successfully logged in")
 
-    # sleep(5)
-    #
-    # # collapse
-    # sidebar = WebDriverWait(driver, 5).until(
-    #     EC.presence_of_element_located((By.ID, 'collapse-button')))
-    #
-    # sidebar.click()
-    #
-    # print("Sidebar has been collapsed")
-    #
-    # devices = WebDriverWait(driver, 2).until(
-    #     EC.presence_of_all_elements_located((By.CSS_SELECTOR, 'button.device-item')))
-    #
-    # current_device = devices[0]
-    # current_device.click()
-    #
-    # chart_tab = WebDriverWait(driver, 5).until(
-    #     EC.presence_of_element_located((By.ID, 'levey-jennings-tab-container')))
-    #
-    # chart_tab.click()
-    #
-    # baselineList = WebDriverWait(driver, 5).until(
-    #     EC.presence_of_element_located((By.ID, 'baseline-tab-btn-lv')))
-    #
-    # baselineList.click()
-    #
-    # driver.get_screenshot_as_file('baseline-tab-expanded.png')
-    #
-    # sleep(5)
-    #
-    # baseline = WebDriverWait(driver, 5).until(
-    #     EC.presence_of_element_located((By.CSS_SELECTOR, '#baseline-tab-btn-lv ul.list-group  li:nth-of-type(2)')))
-    #
-    # baseline.click()
-    #
-    # sleep(5)
-    #
-    # driver.get_screenshot_as_file('baseline-click.png')
-    #
-    # print(driver.current_url)
+    # collapse
+    sidebar = WebDriverWait(driver, 5).until(
+        EC.presence_of_element_located((By.ID, 'collapse-button')))
+
+    sidebar.click()
+
+    passed += 1
+    log(passed, total, "Instrument List sidebar has been successfully collapsed")
+
+    devices = WebDriverWait(driver, 2).until(
+        EC.presence_of_all_elements_located((By.CSS_SELECTOR, 'button.device-item')))
+
+    current_device = devices[0]
+    current_device.click()
+
+    passed += 1
+    log(passed, total, "Device has been successfully selected")
+
+    chart_tab = WebDriverWait(driver, 5).until(
+        EC.presence_of_element_located((By.ID, 'levey-jennings-tab-container')))
+
+    chart_tab.click()
+
+    passed += 1
+    log(passed, total, "User has landed on Levey Jennings module")
+
+    baselineList = WebDriverWait(driver, 5).until(
+        EC.presence_of_element_located((By.ID, 'baseline-tab-btn-lv')))
+
+    baselineList.click()
+
+    passed += 1
+    log(passed, total, "Baseline has been successfully loaded")
+
+    driver.get_screenshot_as_file('baseline-tab-expanded.png')
+
+    sleep(5)
+
+    baseline = WebDriverWait(driver, 5).until(
+        EC.presence_of_element_located((By.CSS_SELECTOR, '#baseline-tab-btn-lv ul.list-group li:nth-of-type(2)')))
+
+    baseline.click()
+
+    passed += 1
+    log(passed, total, "User has successfully selected a baseline from menu")
+
+    sleep(5)
+
+    driver.get_screenshot_as_file('baseline-click.png')
+
+    print(driver.current_url)
 
 
-except Exception as e:
+except BaseException as e:
 
-    print('An error has occurred')
-    print(e)
-
+    error('An error has occurred')
+    error(str(e))
 
 finally:
 
