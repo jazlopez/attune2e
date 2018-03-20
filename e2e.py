6783ab296262c7b5a2309f0316c53bb0385b9f6f
JAZIEL LOPEZ <jaziel.lopez@thermofisher.com>
2018-03-20 11:02:48 -0700

    add resize window to discover hidden elements

--- e2e.py
+++ e2e.py
@@ -14,7 +14,6 @@ config = E2E().config
 
 init()
 
-
 def error(message):
 
     print(Fore.RED + message)
@@ -56,149 +55,147 @@ passed = 0
 
 total = 17
 
-try:
+driver.get(config['url']['login'])
 
-    driver.get(config['url']['login'])
+username = WebDriverWait(driver, 10).until(
+    EC.presence_of_element_located((By.NAME, 'username')))
 
-    username = WebDriverWait(driver, 10).until(
-        EC.presence_of_element_located((By.NAME, 'username')))
+password = WebDriverWait(driver, 10).until(
+    EC.presence_of_element_located((By.NAME, 'password')))
 
-    password = WebDriverWait(driver, 10).until(
-        EC.presence_of_element_located((By.NAME, 'password')))
+submit = WebDriverWait(driver, 10).until(
+    EC.presence_of_element_located((By.CSS_SELECTOR, 'input[type="submit"]')))
 
-    submit = WebDriverWait(driver, 10).until(
-        EC.presence_of_element_located((By.CSS_SELECTOR, 'input[type="submit"]')))
+username.send_keys(config['username'])
+password.send_keys(config['password'])
 
-    username.send_keys(config['username'])
-    password.send_keys(config['password'])
+submit.click()
 
-    submit.click()
+sleep(5)
 
-    sleep(5)
+passed += 1
+log(passed, total, "User has been successfully logged in")
 
-    passed += 1
-    log(passed, total, "User has been successfully logged in")
+driver.get(config['url']['homepage'])
 
-    driver.get(config['url']['homepage'])
+sleep(10)
+# collapse
+sidebar = WebDriverWait(driver, 10).until(
+    EC.presence_of_element_located((By.ID, 'collapse-button')))
 
-    sleep(10)
-    # collapse
-    sidebar = WebDriverWait(driver, 10).until(
-        EC.presence_of_element_located((By.ID, 'collapse-button')))
+sidebar.click()
 
-    sidebar.click()
+passed += 1
+log(passed, total, "Instrument List sidebar has been successfully collapsed")
 
-    passed += 1
-    log(passed, total, "Instrument List sidebar has been successfully collapsed")
+devices = WebDriverWait(driver, 10).until(
+    EC.presence_of_all_elements_located((By.CSS_SELECTOR, 'button.device-item')))
 
-    devices = WebDriverWait(driver, 2).until(
-        EC.presence_of_all_elements_located((By.CSS_SELECTOR, 'button.device-item')))
+current_device = devices[0]
+current_device.click()
 
-    current_device = devices[0]
-    current_device.click()
+sleep(8)
 
-    passed += 1
-    log(passed, total, "Device has been successfully selected")
+passed += 1
+log(passed, total, "Device has been successfully selected")
 
-    chart_tab = WebDriverWait(driver, 10).until(
-        EC.presence_of_element_located((By.ID, 'levey-jennings-tab-container')))
+chart_tab = WebDriverWait(driver, 10).until(
+    EC.presence_of_element_located((By.ID, 'levey-jennings-tab-container')))
 
-    chart_tab.click()
+chart_tab.click()
 
-    sleep(5)
+sleep(5)
 
-    passed += 1
-    log(passed, total, "User has landed on Levey Jennings module")
+passed += 1
+log(passed, total, "User has landed on Levey Jennings module")
 
-    baselineList = WebDriverWait(driver, 10).until(
-        EC.presence_of_element_located((By.ID, 'baseline-tab-btn-lv')))
+baselineList = WebDriverWait(driver, 10).until(
+    EC.presence_of_element_located((By.ID, 'baseline-tab-btn-lv')))
 
-    baselineList.click()
+baselineList.click()
 
-    sleep(5)
+sleep(5)
 
-    passed += 1
-    log(passed, total, "Baselines had been successfully loaded")
+passed += 1
+log(passed, total, "Baselines had been successfully loaded")
 
-    driver.get_screenshot_as_file('baseline-tab-expanded.png')
+driver.get_screenshot_as_file('baseline-tab-expanded.png')
 
-    baseline = WebDriverWait(driver, 10).until(
-        EC.presence_of_element_located((By.CSS_SELECTOR, '#baseline-tab-btn-lv ul.list-group li:nth-of-type(2)')))
+baseline = WebDriverWait(driver, 10).until(
+    EC.presence_of_element_located((By.CSS_SELECTOR, '#baseline-tab-btn-lv ul.list-group li:nth-of-type(2)')))
 
-    baseline.click()
+baseline.click()
 
-    passed += 1
-    log(passed, total, "User has successfully selected a baseline from menu")
-    sleep(5)
+passed += 1
+log(passed, total, "User has successfully selected a baseline from menu")
 
-    ### PLOTTING STATISTICS TESTS ##
-    testById('PlottingStatisticsList', 'plotting-statistics-btn', 5)
-    passed += 1
-    log(passed, total, "Plotting statistics had been successfully loaded")
+# resize
+driver.set_window_size(1450, 600)
 
-    testBySelector('PlottingStatisticsElement', '#plotting-statistics-btn ul.list-group  li:nth-of-type(3)', 5)
-    passed += 1
-    log(passed, total, "User has successfully selected a plotting statistic from menu")
+testById('ButtonDeviation', 'svg_FSC_button_deviation', 5)
+passed +=1
+log(passed, total, "Toggle Deviation")
 
-    testById('ButtonDeviation', 'svg_FSC_button_deviation', 5)
-    passed +=1
-    log(passed, total, "Toggle Deviation")
+testById('ButtonAverage', 'svg_FSC_button_average', 5)
+passed +=1
+log(passed, total, "Toggle Average")
 
-    testById('ButtonAverage', 'svg_FSC_button_average', 5)
-    passed +=1
-    log(passed, total, "Toggle Average")
+### PLOTTING STATISTICS TESTS ##
+testById('PlottingStatisticsList', 'plotting-statistics-btn', 5)
+passed += 1
+log(passed, total, "Plotting statistics had been successfully loaded")
 
-    ### LASER CHANNEL TESTS ##
-    testBySelector('channelsLeveyJennings', '#channels-levey-jennings div.channel-btn.base-channel-btn:nth-child(3)')
-    passed += 1
-    log(passed, total, "User has successfully selected a channel from available channels")
+sleep(5)
+testBySelector('PlottingStatisticsElement', '#plotting-statistics-btn ul.list-group  li:nth-of-type(3)', 5)
+passed += 1
+log(passed, total, "User has successfully selected a plotting statistic from menu")
 
-    ### PAGINATION TESTS ##
-    testBySelector('nextPage', 'div.icon-arrow-right')
-    passed += 1
-    log(passed, total, "User has successfully moved to the next page")
+sleep(5)
+### LASER CHANNEL TESTS ##
+testBySelector('channelsLeveyJennings', '#channels-levey-jennings div:nth-child(3)')
+passed += 1
+log(passed, total, "User has successfully selected a channel from available channels")
 
-    testBySelector('nextPage', 'div.icon-arrow-right')
+sleep(5)
+### PAGINATION TESTS ##
+testBySelector('nextPage', 'div.icon-arrow-right')
+passed += 1
+log(passed, total, "User has successfully moved to the next page")
 
-    #last page
-    #should test the '>' get inactive
-    lastPage = WebDriverWait(driver, 10).until(
-        EC.presence_of_element_located((By.CSS_SELECTOR, 'div.arrow.icon-arrow-right')))
+sleep(5)
+testBySelector('nextPage', 'div.icon-arrow-right')
 
-    passed += 1
-    log(passed, total,"There was unactivated the next page button")
+#last page
+#should test the '>' get inactive
+lastPage = WebDriverWait(driver, 10).until(
+    EC.presence_of_element_located((By.CSS_SELECTOR, 'div.arrow.icon-arrow-right')))
 
-    testBySelector('previousPage', 'div.icon-arrow-left')
-    passed += 1
-    log(passed, total, "User has successfully moved to the previous page")
+passed += 1
+log(passed, total,"There was unactivated the next page button")
 
-    testBySelector('previousPage', 'div.icon-arrow-left')
-    #first page
-    #should test the '<' get inactive
-    firstPage = WebDriverWait(driver, 3).until(
-        EC.presence_of_element_located((By.CSS_SELECTOR, 'div.arrow.icon-arrow-left')))
+sleep(5)
+testBySelector('previousPage', 'div.icon-arrow-left')
+passed += 1
+log(passed, total, "User has successfully moved to the previous page")
 
-    passed += 1
-    log(passed, total, "There was unactivated the first page button")
+testBySelector('previousPage', 'div.icon-arrow-left')
+#first page
+#should test the '<' get inactive
+firstPage = WebDriverWait(driver, 3).until(
+    EC.presence_of_element_located((By.CSS_SELECTOR, 'div.arrow.icon-arrow-left')))
 
-    #Jaziel's code
-    testById('ToggleBarChart', 'view-bar-chart', 5)
-    passed += 1
-    log(passed, total, "User has successfully changed to 2 charts by page")
+passed += 1
+log(passed, total, "There was unactivated the first page button")
+sleep(5)
 
-    testById('ToggleMultipleChart', 'view-grid-chart', 5)
-    passed += 1
-    log(passed, total, "User has successfully changed to 4 charts by page")
+#Jaziel's code
+testById('ToggleBarChart', 'view-bar-chart', 5)
+passed += 1
+log(passed, total, "User has successfully changed to 2 charts by page")
+sleep(5)
 
-    print(driver.current_url)
+testById('ToggleMultipleChart', 'view-grid-chart', 5)
+passed += 1
+log(passed, total, "User has successfully changed to 4 charts by page")
 
-except BaseException as e:
-
-    error('An error has occurred')
-    error(str(e))
-
-finally:
-
-    print("Status {} {}".format(passed, total))
-    print("Finally.. closing web driver")
-    driver.close()
+print(driver.current_url)
